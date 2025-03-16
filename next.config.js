816e70affaedb1fc9419d8a1126ff13228ada996
Mark <marklua599@gmail.com>
2025-03-17 02:43:45 +0800

    initial commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+        pathname: "/storage/**",
+      },
+    ],
+  },};
+
+module.exports = nextConfig;
